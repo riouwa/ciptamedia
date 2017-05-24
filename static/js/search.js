e3f9fc1438d8b82e1e6e06c8a65e5bc7bba6c5e7
Dwi Satria Utama <dwi.satria@wikimedia.or.id>
2017-05-24 12:52:12 +0700

    Update search.js

--- static/js/search.js
+++ static/js/search.js
@@ -7,7 +7,7 @@
 
       for (var i = 0; i < results.length; i++) {  // Iterate over the results
         var item = store[results[i].ref];
-        appendString += '<li><a href="' + item.url + '"><h3>' + item.title + '</h3></a></li>';
+        appendString += '<li><a href="' + item.url + '"><h3>' + item.title + '</h3></a><br/></li>';
       }
 
       searchResults.innerHTML = appendString;
